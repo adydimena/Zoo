3e1316dab2a6bf1d5aa68af25ae19b4ee807185f
adydimena <adydimena10@gmail.com>
2017-11-15 20:12:34 -0500

    change

--- app/src/main/java/com/example/ady/zoo/Recycleadapter.java
+++ app/src/main/java/com/example/ady/zoo/Recycleadapter.java
@@ -3,6 +3,8 @@ package com.example.ady.zoo;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,31 +13,43 @@ import java.util.List;
  * Created by Ady on 11/14/2017.
  */
 
-public class Recycleadapter extends RecyclerView.Adapter<Recycleadapter.View> {
+public class Recycleadapter extends RecyclerView.Adapter<Recycleadapter.ViewHolder> {
     List<String> list = new ArrayList<>();
+
+
 
     public Recycleadapter(List<String> list) {
         this.list = list;
     }
 
     @Override
-    public Recycleadapter.View onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycleviewlayout,null);
+
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(Recycleadapter.View holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (list.get(position) != null);
+        holder.recycleDisplay.setText(list.get(position));
+
 
     }
+
 
     @Override
     public int getItemCount() {
         return list.size();
     }
 
-    public class View extends RecyclerView.ViewHolder {
-        public View(android.view.View itemView) {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView recycleDisplay;
+        public ViewHolder(View itemView) {
             super(itemView);
+            recycleDisplay = itemView.findViewById(R.id.recycleDisplay);
         }
     }
+
 }
